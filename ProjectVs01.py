bd1ee05c29c2dacb786d22fd56e6242b3655f18d
facezazaa <kophakhin155@gmail.com>
2024-02-07 11:30:14 +0700

    a

--- ProjectVs01.py
+++ ProjectVs01.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import pandas as pd 
  
-st.image("https://www.motowish.com/wp-content/uploads/2019/08/%E0%B8%AD%E0%B8%B8%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B9%80%E0%B8%AB%E0%B8%95%E0%B8%B8%E0%B9%83%E0%B8%99%E0%B8%95%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94.jpg")
+st.image("image/12.jpg")
 st.header("การนำเสนอสถิติการเกิดอุบัติเหตุของประเทศไทย")
 
 col1,col2=st.columns(2)
